6f9695f93ad556d2bdd6e589f6b0475607f172fc
AlexRiabchun <olexandrriab364@gmail.com>
2021-04-22 22:29:09 +0300

    4

---
+++ WpfClient/AddDepartment.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDepartment.xaml
+    /// </summary>
+    public partial class AddDepartment : Window
+    {
+        public Department D { get; set; }
+        public AddDepartment(Department d)
+        {
+            InitializeComponent();
+            D = d;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            D.NameDep = AddDepar.Text;
+            this.Close();
+        }
+    }
+}
