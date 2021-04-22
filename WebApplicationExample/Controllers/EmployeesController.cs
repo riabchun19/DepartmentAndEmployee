139e1b7beef35e81cdd6cad596b51bd108983c6a
AlexRiabchun <olexandrriab364@gmail.com>
2021-04-22 22:26:19 +0300

    3

--- WebApplicationExample/Controllers/EmployeesController.cs
+++ WebApplicationExample/Controllers/EmployeesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using WebApplicationExample.Models;
 
 namespace WebApplicationExample.Controllers
@@ -22,18 +24,61 @@ namespace WebApplicationExample.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Employee> Get()
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
         {
-            return db.Employees.ToArray();
+            return await db.Employees.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public Employee Get(int id)
+        public async Task<ActionResult<Employee>> Get(int id)
         {
-            Employee employ = db.Employees.FirstOrDefault(x => x.IdEmp == id);
-            if (employ == null)
-                return employ = null;
-            return employ;
+            Employee employee = await db.Employees.FirstOrDefaultAsync(x => x.IdEmp == id);
+            if (employee == null)
+                return NotFound();
+            return new ObjectResult(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Post(Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+
+            db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Employee>> Put(Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Employees.Any(x => x.IdEmp == employee.IdEmp))
+            {
+                return NotFound();
+            }
+
+            db.Update(employee);
+            await db.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> Delete(int id)
+        {
+            Employee employee = db.Employees.FirstOrDefault(x => x.IdEmp == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return Ok(employee);
         }
     }
 }
